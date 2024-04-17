54986c90f4dc4656fa8d129c4611823828d401f2
NiltonHuamanV <niltonhuaman16@gmail.com>
2024-04-16 20:17:21 -0500

    Eliminación de tabla delivery

--- src/main/java/pe/edu/upc/techsos/repositories/IDeliveryRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.techsos.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.techsos.entities.Delivery;
-
-@Repository
-public interface IDeliveryRepository extends JpaRepository<Delivery, Integer> {
-}
